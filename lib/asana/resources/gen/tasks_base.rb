a9d38f96667a277fc3676d552ee0f2c830e8ad2c
Michael Carey <mikecx@gmail.com>
2024-07-12 13:42:08 -0600

    Remove some testing cruft.

--- lib/asana/resources/gen/tasks_base.rb
+++ lib/asana/resources/gen/tasks_base.rb
@@ -209,7 +209,7 @@ module Asana
         # options - [Hash] the request I/O options
         # > opt_fields - [list[str]]  Defines fields to return. Some requests return *compact* representations of objects in order to conserve resources and complete the request more efficiently. Other times requests return more information than you may need. This option allows you to list the exact set of fields that the API should be sure to return for the objects. The field names should be provided as paths, described below. The id of included objects will always be returned, regardless of the field options.
         # > opt_pretty - [bool]  Provides “pretty” output. Provides the response in a “pretty” format. In the case of JSON this means doing proper line breaking and indentation to make it readable. This will take extra time and increase the response size so it is advisable only to use this during debugging.
-        def get_tasks_for_project(client, project_gid: required("project_gid"), completed_since: nil, per_page: 20, options: { limit: 100 })
+        def get_tasks_for_project(client, project_gid: required("project_gid"), completed_since: nil, per_page: 20, options: {})
           path = "/projects/{project_gid}/tasks"
           path["{project_gid}"] = project_gid
           params = { completed_since:, limit: per_page }.reject { |_,v| v.nil? || Array(v).empty? }
